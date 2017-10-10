54b433b656e536354d3389c9e17b79069b2d45f0
Don McCurdy <don.r.mccurdy@gmail.com>
2017-10-09 22:22:23 -0700

    Add 'tests'.

---
+++ scripts/gen_test.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const chalk = require('chalk');
+
+const VERSION = '2.0';
+const CONTENT_URL = `https://api.github.com/repos/KhronosGroup/glTF-Sample-Models/contents/${VERSION}/`;
+const RAW_BASE_URL = 'https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/';
+const VIEWER_BASE_URL = `http://localhost:3000/#model=${RAW_BASE_URL}${VERSION}/`;
+
+fetch(CONTENT_URL)
+  .then((response) => response.json())
+  .then((directories) => {
+    console.log(chalk.green('Samples:'));
+    directories.forEach((entry) => {
+      const basename = entry.path.split('/').pop();
+      const prettyBasename = chalk.yellow(`${basename}.gltf:`);
+      console.log(` - ${prettyBasename} ${VIEWER_BASE_URL}${basename}/glTF/${basename}.gltf`);
+    });
+    console.log(chalk.black.bgGreen(`\n 🍺  Found ${directories.length} sample models.  \n\n`));
+  });

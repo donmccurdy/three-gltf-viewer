8d3489a0f9d6a5b832db4bdea065cf573667a532
Don McCurdy <don.r.mccurdy@gmail.com>
2017-10-11 19:02:33 -0700

    Electron: Create desktop builds.

--- assets/environment/index.js
+++ assets/environment/index.js
@@ -6,27 +6,27 @@ module.exports = [
   },
   {
     name: 'Park (Day)',
-    path: '/assets/environment/Park2/',
+    path: 'assets/environment/Park2/',
     format: '.jpg'
   },
   {
     name: 'Park (Night)',
-    path: '/assets/environment/Park3Med/',
+    path: 'assets/environment/Park3Med/',
     format: '.jpg'
   },
   {
     name: 'Bridge',
-    path: '/assets/environment/Bridge2/',
+    path: 'assets/environment/Bridge2/',
     format: '.jpg'
   },
   {
     name: 'Sky',
-    path: '/assets/environment/skybox/',
+    path: 'assets/environment/skybox/',
     format: '.jpg'
   },
   {
     name: 'Castle',
-    path: '/assets/environment/SwedishRoyalCastle/',
+    path: 'assets/environment/SwedishRoyalCastle/',
     format: '.jpg'
   }
 ];

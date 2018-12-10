da9229c47e1667d050b21c7ce6eaa9e8bfa9a50e
Don McCurdy <donmccurdy@google.com>
2018-12-10 13:34:49 -0800

    Show author info. Fixes #102.

--- src/validation-controller.js
+++ src/validation-controller.js
@@ -165,6 +165,23 @@ class ValidationController {
       nodesCount: (json.nodes||[]).length,
       texturesCount: (json.textures||[]).length
     });
+
+    if (json.asset && json.asset.extras) {
+      const extras = json.asset.extras;
+      this.report.info.extras = {};
+      if (extras.author) {
+        this.report.info.extras.author = linkify(escapeHTML(extras.author));
+      }
+      if (extras.license) {
+        this.report.info.extras.license = linkify(escapeHTML(extras.license));
+      }
+      if (extras.source) {
+        this.report.info.extras.source = linkify(escapeHTML(extras.source));
+      }
+      if (extras.title) {
+        this.report.info.extras.title = extras.title;
+      }
+    }
   }
 
   /**
@@ -201,6 +218,23 @@ class ValidationController {
     const tab = window.open('', '_blank');
     tab.document.body.innerHTML = this.reportTpl(Object.assign({}, this.report, {location: location}));
   }
+}
+
+function escapeHTML(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+function linkify(text) {
+  const urlPattern = /\b(?:https?):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+  const emailAddressPattern = /(([a-zA-Z0-9_\-\.]+)@[a-zA-Z_]+?(?:\.[a-zA-Z]{2,6}))+/gim;
+  return text
+    .replace(urlPattern, '<a target="_blank" href="$&">$&</a>')
+    .replace(emailAddressPattern, '<a target="_blank" href="mailto:$1">$1</a>');
 }
 
 module.exports = ValidationController;

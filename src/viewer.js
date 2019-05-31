faa02eb5948f3d3af5d2a510b559f9d7bf284b3b
Don McCurdy <dm@donmccurdy.com>
2019-05-31 13:35:49 -0700

    Remove extra updateMatrixWorld call (fixes #152)

--- src/viewer.js
+++ src/viewer.js
@@ -215,7 +215,6 @@ module.exports = class Viewer {
 
     this.clear();
 
-    object.updateMatrixWorld();
     const box = new THREE.Box3().setFromObject(object);
     const size = box.getSize(new THREE.Vector3()).length();
     const center = box.getCenter(new THREE.Vector3());

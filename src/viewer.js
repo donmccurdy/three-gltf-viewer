8c6ad8f05827debe1006e0aa749e2ad2277ba0dc
Don McCurdy <dm@donmccurdy.com>
2022-11-21 21:27:31 -0500

    Add analytics event for preset usage.

--- src/viewer.js
+++ src/viewer.js
@@ -60,6 +60,10 @@ export class Viewer {
     this.clips = [];
     this.gui = null;
 
+    if (options.preset) {
+      Tinybird.trackEvent('preset', {preset: options.preset});
+    }
+
     this.state = {
       environment: options.preset === Preset.ASSET_GENERATOR
         ? environments.find((e) => e.id === 'footprint-court').name

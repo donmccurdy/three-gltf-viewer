a9b912ce8da53aae7623b83b457f27a7ac80f824
Sang Han <sanghan@protonmail.com>
2023-12-12 11:32:44 -0800

    added sm_1.glb file

--- src/objects.js
+++ src/objects.js
@@ -6,5 +6,9 @@ export const objs = [
     {
         filename: "fiesta_tea.glb",
         link: "https://sang-ai.s3.us-west-1.amazonaws.com/glb/fiesta_tea.glb"
+    },
+    {
+        filename: "sm_1.glb",
+        link: "https://sang-ai.s3.us-west-1.amazonaws.com/glb/sm_1.glb"
     }
 ]
